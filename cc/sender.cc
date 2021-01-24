48cf04716b6f0e187d4e0a3a58edca0863faa16d
Ursescu Ionut <ursescuionut97@gmail.com>
2021-01-25 00:00:42 +0200

    Align sender at new api.

--- cc/sender.cc
+++ cc/sender.cc
@@ -14,55 +14,11 @@ int main(int argc, char **argv)
 
 	ll_dev dev;
 
-	ll_dev_init(dev, CacheConfig::CHANNEL_0, CacheConfig::CHANNEL_1);
+	ll_init(dev, CacheConfig::CHANNEL_1, CacheConfig::CHANNEL_0);
 
-	lll_start(dev);
+	std::this_thread::sleep_for(std::chrono::seconds(3));
 
-	ll_pdu recv_pdu;
-	
-	uint8_t count = 1;
-
-	while(true) {
-
-		ll_pdu send_pdu;
-		send_pdu.seq = count;
-		send_pdu.type = PDU_DATA_REQ;
-		send_pdu.data.clear();
-		send_pdu.data.emplace_back(1U);
-		send_pdu.data.emplace_back(0U);
-		send_pdu.data.emplace_back(232U);
-		send_pdu.data.emplace_back(150U);
-		send_pdu.data.emplace_back(200U);
-		send_pdu.data.emplace_back(99U);
-
-		send_pdu.crc = ll_pdu_crc(send_pdu);
-
-		lll_send(dev, send_pdu);
-
-		count++;
-
-		if (count == 0xff) {
-			count = 1U;
-		}
-
-		// std::this_thread::sleep_for(milliseconds(2000));
-
-		// if(lll_recv(dev, recv_pdu)) {
-		// 	std::cout << "Received pdu \n";
-		// 	std::cout << "Seq: " << static_cast<unsigned>(recv_pdu.seq) << "\n";
-		// 	std::cout << "Type: " << static_cast<unsigned>(recv_pdu.type) << "\n";
-		// 	std::cout << "Crc: " << static_cast<unsigned>(recv_pdu.crc) << "\n";
-		// 	std::cout << "Data: ";
-		// 	for (auto byte : recv_pdu.data) {
-		// 		std::cout << " " << static_cast<unsigned>(byte);
-		// 	}
-		// 	std::cout << "\n";
-		// }
-		std::this_thread::sleep_for(milliseconds(30));
-	}
-
-
-	lll_wait_exit(dev);
+	ll_clear(dev);
 
 	return 0;
 }
